481e73ed9442b093b619bae2e48edf40bc78aab0
Thomas Saji <thomasms213@gmail.com>
2022-06-18 08:21:09 +0530

    admin and user header layout added

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  const data = [{
+    product_name: "OPPO A15s",
+    price: 3394,
+    image_url: 'https://m.media-amazon.com/images/I/7136zgtNmJL._SX522_.jpg'
+  }, {
+    product_name: "iQOO Neo 6 5G",
+    price: 1633,
+    image_url: 'https://m.media-amazon.com/images/I/71WS-0ITj7L._SX522_.jpg'
+  }, {
+    product_name: "realme narzo 50",
+    price: 1608,
+    image_url: 'https://m.media-amazon.com/images/I/81gRC3KTeaL._SX522_.jpg'
+  }, {
+    product_name: "TiQOO Z6 Pro 5G",
+    price: 3589,
+    image_url: 'https://m.media-amazon.com/images/I/61E4zA32FrL._SX522_.jpg'
+  }, {
+    product_name: "realme narzo 50A",
+    price: 612,
+    image_url: 'https://m.media-amazon.com/images/I/81Ke5qtC6oL._SX522_.jpg'
+  }, {
+    product_name: "iQOO Z6 44W",
+    price: 1685,
+    image_url: 'https://m.media-amazon.com/images/I/61AFUMyh5QL._SY679_.jpg'
+  }, {
+    product_name: "Tecno Spark 8 Pro",
+    price: 2096,
+    image_url: 'https://m.media-amazon.com/images/I/81Q+kokB8GL._SY679_.jpg'
+  }, {
+    product_name: "Tecno Spark 8T",
+    price: 2274,
+    image_url: 'https://m.media-amazon.com/images/I/71AWvZMY6LL._SX522_.jpg'
+  }, {
+    product_name: "Lava Agni 5G |64 MP AI",
+    price: 1993,
+    image_url: 'https://m.media-amazon.com/images/I/714CC75KiyL._SY679_.jpg'
+  }, {
+    product_name: "Apple iPhone 13",
+    price: 862,
+    image_url: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SX522_.jpg'
+  }]
+  res.render('index', { data, admin: false });
+});
+
+
+module.exports = router;
